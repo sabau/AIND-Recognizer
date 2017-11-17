cde41b4566813216156d7a9a0885509f2448cfe6
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2017-11-17 05:11:31 +0100

    stable recognizer, go back to sleep

--- my_recognizer.py
+++ my_recognizer.py
@@ -1,4 +1,7 @@
 import warnings
+
+import logging
+
 from asl_data import SinglesData
 
 
@@ -20,6 +23,40 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    LOG_FILENAME = 'recognizer.log'
+    log = logging.getLogger('Recognizer')
+    fh = logging.FileHandler(LOG_FILENAME)
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+
+    # Iteate through the test set where i represent the word we are analyzing
+    for i in range(0, len(test_set.get_all_Xlengths())):
+        test_X, test_lengths = test_set.get_item_Xlengths(i)
+        log_l_dict = {}
+        best_score, best_word = float('-Inf'), None
+
+        # try to calculate the probabilities for each word/model and populate the dictionary
+        for word, model in models.items():
+            try:
+                # Try to get the log likelihood of test_X for the current model
+                score = model.score(test_X, test_lengths)
+            except Exception as e:
+                log.warn('EXCEPTION {}'.format(e))
+                # We add this word to maintain the structure of the dictionary,
+                # with probability 0
+                score = float('-Inf')
+            log_l_dict[word] = score
+            log.info("Step {}: logl for word {} is {}".format(i, word, score))
+            # Keep track of the most likely word
+            if score > best_score:
+                log.info("Old score {} for word {} was dethroned by score {} with {} word"
+                         .format(best_score, best_word, score, word))
+                best_score, best_word = score, word
+        # Add the whole dictionary to the probability list
+        probabilities.append(log_l_dict)
+        # store in the guesses the most likely word
+        guesses.append(best_word)
+
+    return probabilities, guesses
